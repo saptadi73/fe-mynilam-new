776025c62abfd6adda1ab62ca593725696b41053
mhaecal <muhaecal@gmail.com>
2024-09-07 17:21:53 +0700

    fix(tailwind): extend color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-    colors: {
-      primary: '#015438',
-      'primary-2': '#7BB401',
+    extend: {
+      colors: {
+        primary: '#015438',
+        'primary-2': '#7BB401',
+      },
     },
   },
   plugins: [],
