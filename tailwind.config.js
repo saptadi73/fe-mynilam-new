351f470c9617041cd33f820bef904e312562e334
mhaecal <muhaecal@gmail.com>
2024-09-07 16:44:55 +0700

    feat(tailwind): setup tailwind color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      primary: '#015438',
+      'primary-2': '#7BB401',
+    },
   },
   plugins: [],
 }
