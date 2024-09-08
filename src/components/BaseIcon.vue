b36ec6c2606a814969fe2ee183140057e3eb1ff7
mhaecal <muhaecal@gmail.com>
2024-09-08 18:22:42 +0700

    feat(icons): add svg icon loader

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <Icon />
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+
+interface Props {
+  name: string
+}
+
+const props = defineProps<Props>()
+
+const Icon = defineAsyncComponent(() => import(`../assets/icons/${props.name}.svg`))
+</script>
--- src/components/BaseTable.vue
+++ src/components/BaseTable.vue
@@ -6,21 +6,7 @@
         <label for="table-search" class="sr-only">Search</label>
         <div class="relative mt-1">
           <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg
-              class="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
+            <BaseIcon name="search" class="w-4 h-4 text-gray-500" />
           </div>
           <input
             type="text"
@@ -67,6 +53,7 @@
 import { createColumnHelper, FlexRender, getCoreRowModel, useVueTable } from '@tanstack/vue-table'
 import axios from 'axios'
 import { onMounted, reactive } from 'vue'
+import BaseIcon from '../components/BaseIcon.vue'
 
 interface DataQuery {
   first_name: string
