def7723d8ebcbd72371be70ab347483ccf144818
mhaecal <muhaecal@gmail.com>
2024-09-08 14:12:02 +0700

    feat(component): add base input float

---
+++ src/components/BaseInputFloat.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- https://flowbite.com/docs/forms/floating-label/#floating-label-example -->
+  <div class="relative z-0 font-poppins">
+    <input
+      :type="type"
+      :id="id"
+      class="block pt-3 pb-1.5 px-0 w-full font-semibold text-primary-2 bg-transparent border-0 border-b-2 border-primary-2 appearance-none focus:outline-none focus:ring-0 focus:border-primary-2 peer"
+      placeholder=" "
+    />
+    <label
+      :for="id"
+      class="absolute text-gray-400 font-semibold duration-300 transform -translate-y-6 scale-90 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-90 peer-focus:-translate-y-6"
+    >
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  label: string
+  id: string
+  type?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  type: 'text',
+})
+</script>
