c63a9d5ea8e942bf53a19433b45cafe6ada2a902
mhaecal <muhaecal@gmail.com>
2024-09-08 14:12:39 +0700

    feat(component): add base checkbox

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex items-center">
+    <input
+      :id="id"
+      type="checkbox"
+      :value="value"
+      class="w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded focus:ring-primary focus:ring-2"
+    />
+    <label :for="id" class="ms-1.5 text-sm text-primary">{{ label }}</label>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  label: string
+  id: string
+  value?: string | number
+}
+
+withDefaults(defineProps<Props>(), {
+  value: '',
+})
+</script>
