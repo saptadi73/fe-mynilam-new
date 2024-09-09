cce471001ec905e979cddc9ef22baf10c18bbc45
ahmdali25 <ahmadaliazhary@gmail.com>
2024-09-09 11:50:32 +0700

    feat(components): add base card component

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="product-card relative bg-[#E6EEEB] rounded-3xl border border-[#B2B4E0] px-5 pt-3 pb-8 w-full">
+    <div class="text-center font-bold text-lg">{{ productCode }}</div>
+
+    <button class="absolute top-2 right-2">
+      <svg width="40" height="40" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <ellipse cx="24.5" cy="25" rx="22.5" ry="22" fill="#E6EEEB" />
+        <path
+          class="hover:fill-[#8ca39b]"
+          d="M13.3327 39.5827L10.416 36.666L22.0827 24.9993L10.416 13.3327L13.3327 10.416L24.9993 22.0827L36.666 10.416L39.5827 13.3327L27.916 24.9993L39.5827 36.666L36.666 39.5827L24.9993 27.916L13.3327 39.5827Z"
+          fill="#B0CAC1"
+        />
+      </svg>
+    </button>
+
+    <div class="flex justify-center pt-2">
+      <img class="rounded-xl" src="../assets/images/produk.jpg" alt="Product Image" />
+    </div>
+    <div class="grid grid-cols-12 pt-2">
+      <div class="col-span-6 pt-2">
+        <h1 class="text-sm">Nama Produk</h1>
+        <p class="font-bold">{{ productName }}</p>
+      </div>
+      <div class="col-span-6 pt-2">
+        <h1 class="text-sm">Nama Penjual</h1>
+        <p class="font-bold">{{ productSellerName }}</p>
+      </div>
+      <div class="col-span-6 pt-2">
+        <h1 class="text-sm">Nama Pembeli</h1>
+        <p class="font-bold">{{ productBuyerName }}</p>
+      </div>
+      <div class="col-span-6 pt-2">
+        <h1 class="text-sm">Jumlah</h1>
+        <p class="font-bold">{{ productQuantity }} kg</p>
+      </div>
+      <div class="col-span-6 pt-2">
+        <h1 class="text-sm">Harga</h1>
+        <p class="font-bold">Rp. {{ productPrice }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface PropsCard {
+  productCode: string
+  productName: string
+  productSellerName: string
+  productBuyerName: string
+  productQuantity: number
+  productPrice: number
+}
+
+withDefaults(defineProps<PropsCard>(), {
+  productCode: 'AGN3K9PT',
+  productName: 'Minyak Nilam',
+  productSellerName: 'Agus Priyatno',
+  productBuyerName: 'Siti Nurhaliza',
+  productQuantity: 3,
+  productPrice: 3000000,
+})
+</script>
+
+<style>
+.product-card {
+  box-shadow: 5px 5px rgba(0, 0, 0, 0.25);
+}
+</style>
