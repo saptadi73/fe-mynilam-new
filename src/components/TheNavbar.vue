c2059dea512d365905a50ed1bc4e494f51431586
mhaecal <muhaecal@gmail.com>
2024-09-11 14:34:31 +0700

    feat(navbar): pop up user profile menu

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,14 +1,37 @@
 <template>
-  <div class="flex items-center justify-between bg-primary py-4 px-8">
+  <div class="flex items-center justify-between space-x-3 bg-primary py-4 px-8">
+    <!-- logo -->
     <div class="font-cera text-white text-2xl"><span class="text-primary-3">MYNIL</span>AM</div>
-    <ul class="hidden lg:flex items-center text-primary bg-white rounded-full py-1.5 px-1">
+    <!-- navbar menu -->
+    <ul class="hidden lg:flex items-center text-primary bg-primary-light rounded-full py-1.5 px-1">
       <li v-for="menu in menuList" class="cursor-pointer">
         <router-link :to="menu.path" class="px-5 py-1 rounded-full transition duration-500">
           {{ menu.name }}
         </router-link>
       </li>
     </ul>
-    <div class="hidden lg:block text-white bg-primary-3 py-2 px-6 rounded-full">John Doe</div>
+    <!-- user profile menu -->
+    <div class="relative">
+      <div class="max-w-[190px] cursor-pointer" @click="showPopUp = !showPopUp">
+        <div
+          class="hidden lg:flex items-center space-x-2 text-white bg-primary-3 hover:bg-[#20D173] p-1.5 rounded-full"
+        >
+          <img src="https://placehold.co/100" class="w-8 h-8 rounded-full border-2 border-white" />
+          <div class="line-clamp-1">Bagas Adi Rukma</div>
+        </div>
+      </div>
+      <!-- pop up -->
+      <div v-if="showPopUp" class="absolute z-50 top-[50px] -left-12 bg-primary-3 rounded-b-md">
+        <div class="flex items-center space-x-2 text-white bg-primary-3 p-3 rounded-full cursor-default">
+          <img src="https://placehold.co/100" class="w-7 h-7 rounded-full border-2 border-white" />
+          <div class="font-semibold">Bagas Adi Rukma</div>
+        </div>
+        <div class="text-center text-white text-sm font-semibold">
+          <div class="py-2 cursor-pointer hover:bg-[#20D173]">View Profile</div>
+          <div class="py-2 cursor-pointer rounded-b-md hover:bg-[#20D173]">Log Out</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,6 +47,8 @@ const menuList = ref([
   { name: 'Penjualan', path: '/penjualan/daftar-penjualan' },
   { name: 'Laporan', path: '/laporan' },
 ])
+
+const showPopUp = ref(false)
 </script>
 
 <style scoped>
