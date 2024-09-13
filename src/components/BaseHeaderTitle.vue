a680a34ceac245e5375f4f1db5ddfcaf7b0edc00
mhaecal <muhaecal@gmail.com>
2024-09-13 11:06:40 +0700

    feat(component): add base header title

---
+++ src/components/BaseHeaderTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="relative flex justify-between items-center w-full pb-8">
+    <div class="absolute left-0" style="justify-self: flex-start">
+      <BaseButton @click="$router.back()" icon-position="left">
+        <BaseIcon name="arrow-left" />
+        Kembali
+      </BaseButton>
+    </div>
+    <h1 class="mx-auto font-semibold text-3xl text-primary">{{ title }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BaseButton from './BaseButton.vue'
+import BaseIcon from './BaseIcon.vue'
+
+interface Props {
+  title: string
+}
+defineProps<Props>()
+</script>
