16e1768de548949e73dddd437cb6b02fe7c28eec
ahmdali25 <ahmadaliazhary@gmail.com>
2024-09-14 22:12:31 +0700

    feat(inventaris): add daftar gudang page

--- src/router/index.ts
+++ src/router/index.ts
@@ -75,6 +75,11 @@ const routes = [
         path: 'inventaris',
         children: [
           {
+            path: 'daftar-gudang',
+            name: 'Daftar Gudang',
+            component: () => import('../pages/inventaris/DaftarGudang.vue'),
+          },
+          {
             path: 'laporan-pergerakan-barang',
             name: 'Laporan Pergerakan Barang',
             component: () => import('../pages/inventaris/LaporanPergerakanBarang.vue'),
