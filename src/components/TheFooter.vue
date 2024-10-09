5d84eed7a185391a78079b296ae680a3934cd7eb
ahmdali25 <ahmadaliazhary@gmail.com>
2024-10-09 20:36:16 +0700

    feat(logo) - change icon, change logo, and add logo on footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,13 +1,16 @@
 <template>
-  <footer class="bg-primary px-5 py-3 flex flex-row justify-between items-center">
-    <p class="text-white 2xl:text-lg">
-      Supported by <span class="font-bold"> <a href="https://www.ilo.org/" target="_blank">ILO</a></span> and
-      <span class="font-bold"> <a href="https://www.seco.admin.ch/seco/en/home.html" target="_blank">SECO</a></span>
-    </p>
-    <div class="flex flex-row gap-x-4">
-      <img class="w-48" src="../assets/images/ilo-logo.svg" alt="ILO Logo" />
-      <div class="bg-white p-1">
-        <img class="w-48" src="../assets/images/seco-logo.png" alt="SECO Logo" />
+  <footer class="bg-primary px-12 py-3 flex flex-row justify-between items-center">
+    <img class="w-48" src="../assets/images/mynilam-logo.png" alt="MyNilam Logo" />
+    <div class="flex flex-col gap-y-2">
+      <p class="text-white text-center 2xl:text-lg">
+        Supported by <span class="font-bold"> <a href="https://www.ilo.org/" target="_blank">ILO</a></span> and
+        <span class="font-bold"> <a href="https://www.seco.admin.ch/seco/en/home.html" target="_blank">SECO</a></span>
+      </p>
+      <div class="flex flex-row gap-x-8">
+        <img class="w-48" src="../assets/images/ilo-logo.svg" alt="ILO Logo" />
+        <div class="bg-white p-1">
+          <img class="w-48" src="../assets/images/seco-logo.png" alt="SECO Logo" />
+        </div>
       </div>
     </div>
   </footer>
