23940c5335f7cdc062abecdc1e5208d313a11f73
ahmdali25 <ahmadaliazhary@gmail.com>
2024-10-11 17:56:50 +0700

    feat(footer) - add responsive footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,12 +1,12 @@
 <template>
-  <footer class="bg-primary px-12 py-3 flex flex-row justify-between items-center">
+  <footer class="bg-primary px-12 py-3 flex flex-col gap-y-4 md:flex-row justify-between items-center">
     <img class="w-48" src="../assets/images/mynilam-logo.png" alt="MyNilam Logo" />
     <div class="flex flex-col gap-y-2">
       <p class="text-white text-center 2xl:text-lg">
         Supported by <span class="font-bold"> <a href="https://www.ilo.org/" target="_blank">ILO</a></span> and
         <span class="font-bold"> <a href="https://www.seco.admin.ch/seco/en/home.html" target="_blank">SECO</a></span>
       </p>
-      <div class="flex flex-row gap-x-8">
+      <div class="flex flex-col gap-y-4 md:flex-row gap-x-8">
         <img class="w-48" src="../assets/images/ilo-logo.svg" alt="ILO Logo" />
         <div class="bg-white p-1">
           <img class="w-48" src="../assets/images/seco-logo.png" alt="SECO Logo" />
