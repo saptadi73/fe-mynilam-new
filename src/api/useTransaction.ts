a8fc9363db53ae72da35e810614c184fabaae57c
mhaecal <muhaecal@gmail.com>
2024-10-20 19:17:53 +0700

    feat(penjualan): filter daftar penjualan

--- src/api/useTransaction.ts
+++ src/api/useTransaction.ts
@@ -1,12 +1,14 @@
 import { useQuery } from '@tanstack/vue-query'
 import { apiGet } from './apiClient'
 import type { DaftarPenjualan, DaftarPenjualanParams } from '@/types/transaction'
+import type { Ref } from 'vue'
 
-export function useDaftarPenjualan(params?: DaftarPenjualanParams) {
+// https://tanstack.com/query/v5/docs/framework/vue/guides/query-keys#if-your-query-function-depends-on-a-variable-include-it-in-your-query-key
+export function useDaftarPenjualan(params?: Ref<DaftarPenjualanParams>) {
   const path = '/transaction/transaction/list'
-  const getDaftarPenjualan = (): Promise<DaftarPenjualan[]> => apiGet(path, params)
+  const getDaftarPenjualan = (): Promise<DaftarPenjualan[]> => apiGet(path, params?.value)
   return useQuery({
-    queryKey: ['daftarPenjualan'],
+    queryKey: ['daftarPenjualan', params],
     queryFn: getDaftarPenjualan,
   })
 }
--- src/pages/penjualan/DaftarPenjualan.vue
+++ src/pages/penjualan/DaftarPenjualan.vue
@@ -12,8 +12,14 @@
             value-key="id"
             :options="kabupaten.data.value"
             placeholder="Pilih Kabupaten"
+            @change="setDaftarPenjualanParams"
           />
-          <BaseInputSelect name="jenis" :options="optionsJenis" placeholder="Pilih Jenis"></BaseInputSelect>
+          <BaseInputSelect
+            name="jenis"
+            :options="optionsJenis"
+            placeholder="Pilih Jenis"
+            @change="setDaftarPenjualanParams"
+          />
         </div>
         <BaseButton variant="success" icon-position="left">
           <BaseIcon name="download" />
@@ -66,101 +72,7 @@
 
       <BaseModal :show-modal="modal" @set-modal="handleModal">
         <template #modal-content>
-          <div class="px-4 md:p-8">
-            <form @submit.prevent="onSubmit" class="space-y-4">
-              <div class="flex justify-center">
-                <div class="flex flex-col">
-                  <div v-if="productImage" class="relative relative-container flex justify-center items-center">
-                    <img class="product-image" :src="productImage" alt="Aset Image" />
-
-                    <svg
-                      @click="handleDeleteProductImage"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="icon-delete hidden absolute w-8 h-8 cursor-pointer z-10"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                      />
-                    </svg>
-                  </div>
-
-                  <div v-else @click="triggerUploadProductImage" class="cursor-pointer">
-                    <div class="flex justify-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        class="text-primary-border w-32 h-32"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
-                          clip-rule="evenodd"
-                        />
-                      </svg>
-                    </div>
-
-                    <h1 class="font-cera font-bold text-xl text-primary-border">Tambah Gambar</h1>
-                  </div>
-                </div>
-
-                <!-- Hidden product image input -->
-                <input
-                  type="file"
-                  name="file"
-                  ref="refProductImage"
-                  accept=".jpg, .jpeg, .png"
-                  @change="handleFileChange"
-                  class="hidden"
-                />
-              </div>
-
-              <hr class="-ml-8 -mr-8 border border-[#015438]" />
-
-              <div class="grid grid-cols-12 gap-x-5">
-                <div class="col-span-6 space-y-4">
-                  <BaseInputFloat label="Nama Penjual" name="nama_penjual" type="text" />
-                  <BaseInputFloat label="Nama Pembeli" name="nama_pembeli" type="text" />
-                  <BaseInputSelect
-                    name="kota"
-                    label-key="name"
-                    value-key="id"
-                    :options="kabupaten.data.value"
-                    placeholder="Kota/Kabupaten"
-                    :floating-label="true"
-                  />
-                  <BaseInputSelect :options="optionsStatus" name="status" placeholder="Status" :floating-label="true" />
-                </div>
-
-                <div class="col-span-6 space-y-4">
-                  <BaseInputSelect :options="optionsJenis" name="jenis" placeholder="Jenis" :floating-label="true" />
-                  <div class="grid grid-cols-12 gap-x-2">
-                    <BaseInputFloat label="Jumlah" name="jumlah" type="text" class="col-span-7" />
-                    <BaseInputSelect
-                      :options="optionsSatuan"
-                      name="satuan"
-                      placeholder="Satuan"
-                      :floating-label="true"
-                      class="col-span-5"
-                    />
-                  </div>
-                  <BaseInputFloat label="Harga/kg" name="harga" type="text" />
-                  <BaseInputFloat label="Sub total" name="sub_total" type="text" />
-                </div>
-              </div>
-
-              <div class="flex justify-center gap-x-4 mx-8">
-                <BaseButton type="submit" class="w-full font-bold">Simpan</BaseButton>
-                <BaseButton @click="closeModal" variant="success" class="w-full font-bold">Kembali</BaseButton>
-              </div>
-            </form>
-          </div>
+          <FormTambahDaftarProduk @close-modal="closeModal" />
         </template>
       </BaseModal>
 
@@ -190,22 +102,38 @@ import BaseHeaderTitle from '@/components/BaseHeaderTitle.vue'
 import BaseIcon from '@/components/BaseIcon.vue'
 import BaseCardAdd from '@/components/BaseCardAdd.vue'
 import BaseModal from '@/components/BaseModal.vue'
-import BaseInputFloat from '@/components/BaseInputFloat.vue'
+import FormTambahDaftarProduk from './components/FormTambahDaftarProduk.vue'
 
 import { useQRCode } from '@vueuse/integrations/useQRCode'
 import { ref } from 'vue'
-import { useForm } from 'vee-validate'
-import { object, string, number } from 'yup'
-import type { ProdukNilamType } from '@/types/produk'
 import { useKabupaten } from '@/api/useLocalization'
 import { useDaftarPenjualan } from '@/api/useTransaction'
+import { useForm } from 'vee-validate'
+import type { DaftarPenjualanParams } from '@/types/transaction'
+
+interface Form {
+  kabupaten: DaftarPenjualanParams['kabupaten_id']
+  jenis: DaftarPenjualanParams['associate_type']
+}
+
+const { values } = useForm<Form>()
 
 let modal = ref<Boolean>(false)
 let modalQr = ref<Boolean>(false)
 let qrcode = ref<string>('')
 
 const kabupaten = useKabupaten()
-const daftarPenjualan = useDaftarPenjualan({ kabupaten_id: 1 })
+
+// auto refetch on daftarPenjualanParams change
+const daftarPenjualanParams = ref<DaftarPenjualanParams>({})
+const daftarPenjualan = useDaftarPenjualan(daftarPenjualanParams)
+
+const setDaftarPenjualanParams = () => {
+  daftarPenjualanParams.value = {
+    kabupaten_id: values.kabupaten,
+    associate_type: values.jenis,
+  }
+}
 
 const showModal = () => {
   modal.value = true
@@ -252,66 +180,10 @@ const downloadQrCodeImage = () => {
   URL.revokeObjectURL(link.href) // Bersihkan URL setelah digunakan
 }
 
-const { handleSubmit } = useForm<ProdukNilamType>({
-  validationSchema: object({
-    nama_penjual: string().required().label('Nama Penjual'),
-    jenis: number().required().label('Jenis'),
-    nama_pembeli: string().required().label('Nama Pembeli'),
-    jumlah: number().required().label('Jumlah'),
-    kota: string().required().label('Kota/Kabupaten'),
-    harga: string().required().label('Harga/kg'),
-    status: string().required().label('Status'),
-    sub_total: number().required().label('Sub total'),
-  }),
-})
-
-// const getLabelByValue = (optionsArray: { label: string; value: any }[], value: string | null) => {
-//   const option = optionsArray.find((opt) => opt.value === value)
-//   return option ? option.label : ''
-// }
-
-const onSubmit = handleSubmit((_values) => {
-  // values.jenis = getLabelByValue(optionsJenis.value, values.jenis)
-  // values.kota = getLabelByValue(optionsKabupaten.value, values.kota)
-  // values.status = getLabelByValue(optionsStatus.value, values.status)
-  // values.kode = `000-MN-000-${values.sub_total.toString().slice(0, 3)}-00${daftarNilam.length + 1}`
-
-  // daftarNilam.push(values)
-  closeModal()
-})
-
-const optionsSatuan = ref([{ label: 'Kg', value: 1 }])
-
 const optionsJenis = ref([
-  { label: 'Agen', value: 1 },
-  { label: 'Koperasi', value: 2 },
+  { label: 'Agen', value: 'agent' },
+  { label: 'Koperasi', value: 'koperasi' },
 ])
-
-const optionsStatus = ref([
-  { label: 'Tersedia', value: 1 },
-  { label: 'Menunggu', value: 2 },
-])
-
-const refProductImage = ref<HTMLInputElement | null>(null)
-const productImage = ref<string | null>(null)
-
-const triggerUploadProductImage = () => {
-  refProductImage.value?.click()
-}
-
-const handleFileChange = (event: Event) => {
-  const target = event.target as HTMLInputElement
-  const file = target.files?.[0]
-  if (file) {
-    // Handle the file upload or processing here
-    const fileURL = URL.createObjectURL(file)
-    productImage.value = fileURL
-  }
-}
-
-const handleDeleteProductImage = () => {
-  productImage.value = null
-}
 </script>
 
 <style scoped>
