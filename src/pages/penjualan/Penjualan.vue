a92ec2f14bbbcfc9044847c375a2c9bdf1d1478a
mhaecal <muhaecal@gmail.com>
2024-11-07 12:16:21 +0700

    fix(penjualan): remove mitra section

--- src/pages/penjualan/Penjualan.vue
+++ src/pages/penjualan/Penjualan.vue
@@ -14,27 +14,6 @@
       daftar pelanggan, dan menampilkan pesanan atau penjualan terbaru dengan rincian transaksi.
     </p>
 
-    <section
-      class="my-20 md:my-10 md:p-8 text-primary flex flex-col-reverse md:flex-row items-start md:space-x-10 w-full"
-    >
-      <div class="md:w-5/12">
-        <img src="@/assets/images/penjualan/penjualan-thumbnail-1.png" class="rounded-3xl w-full" />
-      </div>
-      <div class="md:w-7/12 py-3 space-y-5">
-        <h1 class="text-3xl font-bold">Mitra</h1>
-        <p class="text-lg">
-          Jelajahi daftar gudang yang menyimpan hasil penjualan petani nilam, memastikan kualitas produk tetap terjaga
-          sebelum didistribusikan ke agen atau koperasi.
-        </p>
-        <RouterLink :to="{ name: 'Daftar Produksi Petani' }">
-          <BaseButton icon-position="left">
-            Lihat Pelanggan
-            <BaseIcon name="chevron-right" class="w-4 h-4" />
-          </BaseButton>
-        </RouterLink>
-      </div>
-    </section>
-
     <section class="my-20 md:my-10 md:p-8 text-primary flex flex-col md:flex-row items-start md:space-x-10 w-full">
       <div class="md:w-6/12 py-3 space-y-5">
         <h1 class="text-3xl font-bold">Daftar Penjualan</h1>
@@ -61,12 +40,13 @@
         <img src="@/assets/images/penjualan/penjualan-thumbnail-3.png" class="rounded-3xl w-full" />
       </div>
       <div class="md:w-7/12 py-3 space-y-5">
-        <h1 class="text-3xl font-bold">Laporan Pergerakan Barang</h1>
+        <h1 class="text-3xl font-bold">Laporan Penjualan dan Tracebility Product</h1>
         <p class="text-lg">
-          Lacak perjalanan minyak nilam mulai dari petani, kemudian dikirim ke agen atau koperasi, dilanjutkan ke
-          UGreen, hingga akhirnya sampai di Green.
+          Menampilkan data penjualan lengkap dari setiap transaksi, mulai dari agen, koperasi, hingga Ugreen, serta
+          memantau pergerakan produk secara menyeluruh. Laporan ini mencakup riwayat penjualan, jalur distribusi, dan
+          ketepatan waktu pengiriman, sehingga memastikan transparansi produk dan meningkatkan kepercayaan Ugreen.
         </p>
-        <RouterLink :to="{ name: 'Laporan Produksi' }">
+        <RouterLink :to="{ name: 'Pilih Jenis Laporan Penjualan' }">
           <BaseButton icon-position="left">
             Lihat Pergerakan
             <BaseIcon name="chevron-right" class="w-4 h-4" />
