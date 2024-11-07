f9756456a848442e5f9729af38f09576ac60568b
mhaecal <muhaecal@gmail.com>
2024-11-07 12:17:53 +0700

    feat(penjualan): update flow pergerakan barang

---
+++ src/pages/penjualan/laporan-penjualan/PilihLaporanPenjualan.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-image-wave px-16">
+    <BaseProductCard card-title="Pilih Laporan" :card-content="cardContent" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BaseProductCard from '@/components/BaseProductCard.vue'
+
+const cardContent = [
+  {
+    title: 'Petani',
+    content:
+      'Menampilkan data penjualan petani, rincian ke agen/koperasi, serta analisis tren dari tanam hingga distribusi.',
+    pathName: 'Laporan Penjualan Petani',
+  },
+  {
+    title: 'Agen/Koperasi',
+    content:
+      ' Menampilkan data penjualan, rincian distribusi, serta analisis perkembangan penjualan dari agen dan koperasi ke Ugreen.',
+    pathName: 'Laporan Penjualan Agen Koperasi',
+  },
+]
+</script>
