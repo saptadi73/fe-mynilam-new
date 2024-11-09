175045544aee8ace46a04d1769a663f02c514e8b
ahmdali25 <ahmadaliazhary@gmail.com>
2024-11-09 20:43:12 +0700

    feat(produksi) - API integration daftar produksi nilam

--- src/pages/produksi/DaftarProduksiNilam.vue
+++ src/pages/produksi/DaftarProduksiNilam.vue
@@ -4,7 +4,14 @@
     <div class="bg-[#F6FDFF] p-4 rounded-3xl border border-[#015438]">
       <div class="grid grid-cols-12 gap-4">
         <BaseCardAdd card-title="Produksi" class="col-span-3" />
-        <BaseCard v-for="(card, cardIndex) in cardPetani" :key="cardIndex" :card-code="card.code" class="col-span-3">
+        <BaseSkeletonCard v-if="isLoading" v-for="n in 3" :key="n" class="col-span-12 md:col-span-6 lg:col-span-3" />
+        <BaseCard
+          v-else
+          v-for="(card, cardIndex) in daftarProduksiNilam"
+          :key="cardIndex"
+          :card-code="card.production_identifier"
+          class="col-span-3"
+        >
           <template #card-content>
             <div class="grid grid-cols-12 items-center gap-x-2">
               <div class="col-span-6">
@@ -28,35 +35,35 @@
             <div class="grid grid-cols-12 gap-x-1 pt-2">
               <div class="col-span-6 pt-2">
                 <h1 class="text-sm">Nama Petani</h1>
-                <p class="font-bold text-sm">{{ card.petaniName }}</p>
+                <p class="font-bold text-sm">{{ card.farmer_name }}</p>
               </div>
               <div class="col-span-6 pt-2">
                 <h1 class="text-sm">Lokasi</h1>
-                <p class="font-bold text-sm">{{ card.lokasi }} m²</p>
+                <p class="font-bold text-sm">{{ card.coordinates }}</p>
               </div>
               <div class="col-span-6 pt-2">
                 <h1 class="text-sm">Mulai Produksi</h1>
-                <p class="font-bold text-sm">{{ card.mulaiProduksi }}</p>
+                <p class="font-bold text-sm">{{ formatDate(card.date_started) }}</p>
               </div>
               <div class="col-span-6 pt-2">
                 <h1 class="text-sm">Akhir Produksi</h1>
-                <p class="font-bold text-sm">{{ card.akhirProduksi }}</p>
+                <p class="font-bold text-sm">{{ formatDate(card.date_harvested) }}</p>
               </div>
               <div class="col-span-6 pt-2">
-                <h1 class="text-sm">Estimasi Panen</h1>
-                <p class="font-bold text-sm">{{ card.estimasiPanen }} kg</p>
+                <h1 class="text-sm">Estimasi Produksi</h1>
+                <p class="font-bold text-sm">{{ card.production_estimates }} kg</p>
               </div>
               <div class="col-span-6 pt-2">
                 <h1 class="text-sm">Alamat</h1>
-                <p class="font-bold text-sm">{{ card.alamat }}</p>
+                <p class="font-bold text-sm">{{ card.street }}</p>
               </div>
               <div class="col-span-6 pt-2">
                 <h1 class="text-sm">Status</h1>
                 <p
-                  v-if="card.status == 'progress'"
-                  class="bg-[#015438] text-white font-bold text-sm rounded-lg inline-block px-2.5 py-1"
+                  :class="{ 'bg-[#20D173]': card.state === 'draft', 'bg-[#015438]': card.state === 'done' }"
+                  class="text-white font-bold text-sm capitalize rounded-lg inline-block px-2.5 py-1"
                 >
-                  On Progress
+                  {{ card.state }}
                 </p>
               </div>
             </div>
@@ -73,8 +80,10 @@ import BaseHeaderTitle from '@/components/BaseHeaderTitle.vue'
 import BaseCardAdd from '@/components/BaseCardAdd.vue'
 import BaseCard from '@/components/BaseCard.vue'
 import BaseChart from '@/components/BaseChart.vue'
-
-import { reactive } from 'vue'
+import BaseSkeletonCard from '@/components/BaseSkeletonCard.vue'
+import { onMounted, reactive, ref } from 'vue'
+import { useHttp } from '@/api/useHttp'
+import { formatDate } from '../../utils/useFormatDate'
 
 const chartData: ChartData = {
   labels: ['', 'Proses'],
@@ -98,83 +107,36 @@ const chartOptions: ChartOptions<'doughnut'> = {
   },
 }
 
-const cardPetani = reactive([
-  {
-    id: 1,
-    code: 'PRD1X7QF',
-    petaniName: 'Agus Prayitno',
-    lokasi: '4.747883, 96.748811',
-    mulaiProduksi: '17 April 2024',
-    akhirProduksi: '17 Agustus 2024',
-    estimasiPanen: '5000',
-    alamat: 'Timang Rasa, Timang Gajah, Bener Meriah Regency, Aceh',
-    status: 'progress',
-  },
-  {
-    id: 2,
-    code: 'PRD1X7QF',
-    petaniName: 'Budi Santoso',
-    lokasi: '4.747883, 96.748811',
-    mulaiProduksi: '17 April 2024',
-    akhirProduksi: '17 Agustus 2024',
-    estimasiPanen: '5000',
-    alamat: 'Timang Rasa, Timang Gajah, Bener Meriah Regency, Aceh',
-    status: 'progress',
-  },
-  {
-    id: 3,
-    code: 'PRD1X7QF',
-    petaniName: 'Rika Kusuma',
-    lokasi: '4.747883, 96.748811',
-    mulaiProduksi: '17 April 2024',
-    akhirProduksi: '17 Agustus 2024',
-    estimasiPanen: '5000',
-    alamat: 'Timang Rasa, Timang Gajah, Bener Meriah Regency, Aceh',
-    status: 'progress',
-  },
-  {
-    id: 4,
-    code: 'PRD1X7QF',
-    petaniName: 'Rika Kusuma',
-    lokasi: '4.747883, 96.748811',
-    mulaiProduksi: '17 April 2024',
-    akhirProduksi: '17 Agustus 2024',
-    estimasiPanen: '5000',
-    alamat: 'Timang Rasa, Timang Gajah, Bener Meriah Regency, Aceh',
-    status: 'progress',
-  },
-  {
-    id: 5,
-    code: 'PRD1X7QF',
-    petaniName: 'Rika Kusuma',
-    lokasi: '4.747883, 96.748811',
-    mulaiProduksi: '17 April 2024',
-    akhirProduksi: '17 Agustus 2024',
-    estimasiPanen: '5000',
-    alamat: 'Timang Rasa, Timang Gajah, Bener Meriah Regency, Aceh',
-    status: 'progress',
-  },
-  {
-    id: 6,
-    code: 'PRD1X7QF',
-    petaniName: 'Rika Kusuma',
-    lokasi: '4.747883, 96.748811',
-    mulaiProduksi: '17 April 2024',
-    akhirProduksi: '17 Agustus 2024',
-    estimasiPanen: '5000',
-    alamat: 'Timang Rasa, Timang Gajah, Bener Meriah Regency, Aceh',
-    status: 'progress',
-  },
-  {
-    id: 7,
-    code: 'PRD1X7QF',
-    petaniName: 'Rika Kusuma',
-    lokasi: '4.747883, 96.748811',
-    mulaiProduksi: '17 April 2024',
-    akhirProduksi: '17 Agustus 2024',
-    estimasiPanen: '5000',
-    alamat: 'Timang Rasa, Timang Gajah, Bener Meriah Regency, Aceh',
-    status: 'progress',
-  },
-])
+interface NilamType {
+  id: number
+  production_identifier: string
+  farmer_name: string
+  coordinates: string
+  date_started: string
+  date_harvested: string
+  production_estimates: string
+  street: string
+  state: string
+}
+
+let daftarProduksiNilam = reactive<NilamType[]>([])
+const isLoading = ref<boolean>(false)
+
+const getProduksiNilam = async () => {
+  isLoading.value = true
+  const response = await useHttp('/production/harvesting/list')
+  const nilamData = await response.data
+
+  daftarProduksiNilam = nilamData.map((nilam: { employee_id: any; final_product: any }) => ({
+    ...nilam,
+    farmer_name: nilam.employee_id !== false ? nilam.employee_id[1] : null,
+    production_estimates: nilam.final_product[0],
+  }))
+
+  isLoading.value = false
+}
+
+onMounted(() => {
+  getProduksiNilam()
+})
 </script>
---
+++ src/utils/useFormatDate.ts
@@ -0,0 +1,12 @@
+export function formatDate(dateString: string | number | Date) {
+  const date = new Date(dateString)
+
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+  }
+
+  // Format menggunakan Intl.DateTimeFormat
+  return new Intl.DateTimeFormat('id-ID', options).format(date)
+}
