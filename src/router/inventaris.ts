3aeface9695a34593c2e3462130bd0d2bafd28a0
mhaecal <muhaecal@gmail.com>
2024-11-09 12:59:57 +0700

    fix(inventaris): remove daftar gudang

--- src/router/inventaris.ts
+++ src/router/inventaris.ts
@@ -7,36 +7,6 @@ export default {
       component: () => import('../pages/inventaris/PilihInventaris.vue'),
     },
     {
-      path: 'daftar-produk',
-      name: 'Daftar Produk Minyak Nilam',
-      component: () => import('../pages/inventaris/DaftarProduk.vue'),
-    },
-    {
-      path: 'daftar-gudang',
-      children: [
-        {
-          path: '',
-          name: 'Daftar Gudang',
-          component: () => import('../pages/inventaris/DaftarGudang.vue'),
-        },
-        {
-          path: 'agen',
-          name: 'Agen',
-          component: () => import('../pages/inventaris/gudang/Agen.vue'),
-        },
-        {
-          path: 'koperasi',
-          name: 'Koperasi',
-          component: () => import('../pages/inventaris/gudang/Koperasi.vue'),
-        },
-        {
-          path: 'ugreen',
-          name: 'Ugreen',
-          component: () => import('../pages/inventaris/gudang/UGreen.vue'),
-        },
-      ],
-    },
-    {
       path: 'laporan-pergerakan-barang',
       name: 'Laporan Pergerakan Barang',
       component: () => import('../pages/inventaris/LaporanPergerakanBarang.vue'),
