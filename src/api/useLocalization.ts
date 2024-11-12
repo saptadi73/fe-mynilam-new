689dab36ee6090877d9d4344b9fb6eddda2ae5cc
ahmdali25 <ahmadaliazhary@gmail.com>
2024-11-12 18:41:23 +0700

    feat(useKabupaten) - change localizaton kabupaten to aceh

--- src/api/useLocalization.ts
+++ src/api/useLocalization.ts
@@ -3,7 +3,8 @@ import { apiGet } from './apiClient'
 import type { Kabupaten } from '@/types/localization'
 
 export function useKabupaten() {
-  const path = '/localization/kabupaten'
+  // id prov aceh
+  const path = '/localization/kabupaten_by_provinsi_id/613'
   const getKabupaten = (): Promise<Kabupaten[]> => apiGet(path)
   return useQuery({
     queryKey: ['kabupaten'],
