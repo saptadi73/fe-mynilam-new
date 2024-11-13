0b894e6ad6d2137869b014b20dc02fb8827443d0
ahmdali25 <ahmadaliazhary@gmail.com>
2024-11-13 11:03:44 +0700

    feat(components) - add base pilih daftar petani component

--- src/pages/profile/PilihDaerahPetani.vue
+++ src/pages/profile/PilihDaerahPetani.vue
@@ -1,38 +1,7 @@
 <template>
-  <div class="container bg-primary-light pb-14">
-    <BaseHeaderTitle title="Pilih Daerah Petani Nilam" />
-    <BaseMap shp-file="/maps/aceh_kab.zip" :options="mapOptions" />
-  </div>
+  <BasePilihDaerahPetani router-name="Daftar Petani" />
 </template>
 
 <script setup lang="ts">
-import BaseHeaderTitle from '@/components/BaseHeaderTitle.vue'
-import BaseMap from '@/components/BaseMap.vue'
-import type { GeoJSONOptions } from 'leaflet'
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-
-const getRandomColor = () => {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16)
-}
-
-const mapOptions: GeoJSONOptions = {
-  onEachFeature: function (feature, layer) {
-    layer.on({
-      click: () => {
-        router.push({ name: 'Daftar Petani', params: { daerah: feature.properties['Kab_Kota'] } })
-      },
-      mouseover: () => {
-        layer.bindPopup(feature.properties['Kab_Kota']).openPopup()
-      },
-    })
-  },
-  style: function () {
-    return {
-      fillColor: getRandomColor(),
-      fillOpacity: 0.6,
-    }
-  },
-}
+import BasePilihDaerahPetani from '@/components/BasePilihDaerahPetani.vue'
 </script>
