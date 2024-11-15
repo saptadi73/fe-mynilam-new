aee0c3e188c576342a7f091556a88666e2f1ae28
ahmdali25 <ahmadaliazhary@gmail.com>
2024-11-15 17:14:24 +0700

    feat(beranda) - add blur thumbnail image

--- src/pages/Beranda.vue
+++ src/pages/Beranda.vue
@@ -199,6 +199,6 @@ import BaseIcon from '@/components/BaseIcon.vue'
 
 <style scoped>
 .thumbnail-image {
-  box-shadow: 6px 6px rgba(0, 0, 0, 0.25);
+  box-shadow: 10px 10px 4px 0 rgba(0, 0, 0, 0.25);
 }
 </style>
