d4ebb75aa8598654f7be30349a253fcccd6ee71c
mhaecal <muhaecal@gmail.com>
2024-11-29 09:03:24 +0700

    feat(pilih-penjualan): add daftar penjualan

--- src/pages/penjualan/PilihJenisLaporan.vue
+++ src/pages/penjualan/PilihJenisLaporan.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="bg-image-wave px-5 md:px-16">
-    <BaseHeaderTitle title="Pilih Jenis Laporan" :back-button="false" />
-    <div class="bg-[#F6FDFF] rounded-xl shadow-lg p-8 grid grid-cols-12 gap-y-8 md:gap-y-0 md:gap-x-12">
-      <router-link :to="{ name: 'Pilih Daerah Penjualan' }" class="col-span-12 md:col-span-4 md:col-start-3">
+    <BaseHeaderTitle title="Pilih Penjualan" :back-button="false" />
+    <div class="bg-[#F6FDFF] rounded-xl shadow-lg p-8 grid grid-cols-12 gap-y-8 md:gap-y-0 md:gap-x-8">
+      <router-link :to="{ name: 'Daftar Penjualan' }" class="col-span-12 md:col-span-4">
+        <div
+          class="mitra-card bg-[#DEFCEC] hover:bg-slate-200 transition-transform hover:scale-105 duration-300 ease-in-out rounded-2xl cursor-pointer pb-20"
+        >
+          <img src="@/assets/images/penjualan/mitra-agen.png" alt="Mitra Agen" />
+          <h1 class="text-3xl text-primary font-cera font-bold text-center">Daftar Penjualan</h1>
+        </div>
+      </router-link>
+      <router-link :to="{ name: 'Pilih Daerah Penjualan' }" class="col-span-12 md:col-span-4">
         <div
           class="mitra-card bg-[#DEFCEC] hover:bg-slate-200 transition-transform hover:scale-105 duration-300 ease-in-out rounded-2xl cursor-pointer pb-20"
         >
@@ -10,7 +18,7 @@
           <h1 class="text-3xl text-primary font-cera font-bold text-center">Laporan Penjualan</h1>
         </div>
       </router-link>
-      <router-link :to="{ name: 'Pilih Traceability Product' }" class="col-span-12 md:col-span-4 md:col-start-7">
+      <router-link :to="{ name: 'Pilih Traceability Product' }" class="col-span-12 md:col-span-4">
         <div
           class="mitra-card bg-[#DEFCEC] hover:bg-slate-200 transition-transform hover:scale-105 duration-300 ease-in-out rounded-2xl cursor-pointer pb-20"
         >
