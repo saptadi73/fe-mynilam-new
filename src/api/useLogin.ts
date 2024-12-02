cc8e90d8fdf137a7c8c80aee78e67e978c630c6f
ahmdali25 <ahmadaliazhary@gmail.com>
2024-12-02 18:19:35 +0700

    feat(api-post): fix headers ts

--- src/api/useLogin.ts
+++ src/api/useLogin.ts
@@ -1,15 +1,16 @@
 import { useMutation, useQuery } from '@tanstack/vue-query'
 import { apiGet, apiPost } from './apiClient'
 import type { LoginAuth, LoginForm, User } from '@/types/login'
+import { AxiosHeaders } from 'axios'
 
 /**
  * Login token
  */
 export function useLogin() {
   const path = '/login_auth/token'
-  const headers = {
+  const headers = new AxiosHeaders({
     'Content-Type': 'application/x-www-form-urlencoded',
-  }
+  })
   const loginFn = (form: LoginForm): Promise<LoginAuth> => apiPost(path, form, headers)
   return useMutation({
     mutationFn: loginFn,
