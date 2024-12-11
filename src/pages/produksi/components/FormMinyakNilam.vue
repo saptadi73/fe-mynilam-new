70fa00b7f57fa473287ba445f44a4a1c6965a8ac
mhaecal <muhaecal@gmail.com>
2024-12-11 10:49:25 +0700

    feat(buat-produksi): add input image

--- src/pages/produksi/components/FormMinyakNilam.vue
+++ src/pages/produksi/components/FormMinyakNilam.vue
@@ -1,6 +1,58 @@
 <template>
   <form class="md:w-1/2 pl-7" @submit="onSubmit">
     <h1 class="text-primary text-2xl font-bold text-center pb-5">Minyak Nilam</h1>
+    <div class="flex justify-center pb-4 border-2 border-primary translate-y-2 border-b-0 rounded-t-lg">
+      <div class="flex flex-col">
+        <div v-if="productImage" class="relative relative-container flex justify-center items-center">
+          <img :src="productImage" />
+
+          <svg
+            @click="handleDeleteProductImage"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="icon-delete hidden absolute w-8 h-8 cursor-pointer z-10"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+            />
+          </svg>
+        </div>
+
+        <div v-else @click="triggerUploadProductImage" class="cursor-pointer">
+          <div class="flex justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="text-primary-border w-32 h-32"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+
+          <h1 class="font-cera font-bold text-xl text-primary-border">Tambah Gambar</h1>
+        </div>
+      </div>
+
+      <!-- Hidden product image input -->
+      <input
+        type="file"
+        name="file"
+        ref="refProductImage"
+        accept=".jpg, .jpeg, .png"
+        @change="handleFileChange"
+        class="hidden"
+      />
+    </div>
     <div class="md:grid md:grid-cols-2 gap-10 border-2 border-primary rounded-lg py-7 px-10">
       <BaseInputFloat type="text" label="Nama" name="nama" />
       <BaseInputFloat type="text" label="Lokasi" name="lokasi" />
@@ -47,11 +99,32 @@ const { handleSubmit } = useForm({
   initialValues: {},
 })
 
+const refProductImage = ref<HTMLInputElement | null>(null)
+const productImage = ref<string | null>(null)
+
 const options = ref([
   { label: 'Agus Nur Drajat', value: 1 },
   { label: 'Jayadi Idzes', value: 2 },
   { label: 'Martin Paes', value: 3 },
 ])
+
+const handleDeleteProductImage = () => {
+  productImage.value = null
+}
+
+const triggerUploadProductImage = () => {
+  refProductImage.value?.click()
+}
+
+const handleFileChange = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  const file = target.files?.[0]
+  if (file) {
+    // Handle the file upload or processing here
+    const fileURL = URL.createObjectURL(file)
+    productImage.value = fileURL
+  }
+}
 
 const onSubmit = handleSubmit((values) => {
   console.log(values)
