95f2e4fc3d658c4e1a8ea08cf1ebe536468e9bd6
mhaecal <muhaecal@gmail.com>
2025-01-13 23:34:44 +0700

    feat(api): add lov lahan api

--- src/api/useLov.ts
+++ src/api/useLov.ts
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/vue-query'
 import { Ref } from 'vue'
 import { apiGet } from './apiClient'
 import { Petani, PetaniListParams } from '@/types/partner'
+import { Lahan } from '@/types/lov'
 
 export function useLovPetani(params?: Ref<PetaniListParams>) {
   const path = '/lov/list-petani'
@@ -31,5 +32,15 @@ export function useLovUOM() {
   return useQuery({
     queryKey: ['lovUom'],
     queryFn: getLovUOM,
+  })
+}
+
+export function useLovLahan(params?: Ref<{ id_petani?: number }>) {
+  const path = '/lov/lov-lahan'
+  const getLovLahan = (): Promise<Lahan[]> => apiGet(path, params)
+  return useQuery({
+    queryKey: ['lovLahan'],
+    queryFn: getLovLahan,
+    enabled: () => !!params?.value.id_petani,
   })
 }
---
+++ src/types/lov.ts
@@ -0,0 +1,8 @@
+export interface Lahan {
+  id: number
+  name: string
+  kabupaten_id: [number, string]
+  employee_id: [number, string]
+  coordinates: string
+  address: string
+}
